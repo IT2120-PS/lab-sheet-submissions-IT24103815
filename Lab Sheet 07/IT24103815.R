d855502369dbce42cd6010100aafa58256ec632d
IT24103815 <it24103815@my.sliit.lk>
2025-09-16 23:11:50 +0530

    IT24103815.R

---
+++ Lab Sheet 07/IT24103815.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\saths\\OneDrive\\Desktop\\IT24103815")
+
+#Q1
+punif(10, min=0, max=40, lower.tail=FALSE) - punif(25, min=0, max=40, lower.tail=FALSE)
+
+#Q2
+pexp(0, rate=1/3, lower.tail=FALSE) - pexp(2, rate=1/3, lower.tail=FALSE)
+
+#Q3.1
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+
+#Q3.2
+qnorm(0.05, mean=100, sd=15, lower.tail=FALSE)
